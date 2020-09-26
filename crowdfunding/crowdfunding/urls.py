90880ed98d2a575660d0006f4df60b4d0f936879
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-09-26 12:26:43 +1000

    adding views to crowdfunding

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view()),
     path('', include ('projects.urls')),
     path('', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
